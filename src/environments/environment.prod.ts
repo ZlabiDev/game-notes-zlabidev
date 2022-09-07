400264741af2a9498afb969fdc80258784679c93
zlabi_5 <daniel.zlabinger@gmx.at>
2022-09-07 19:50:30 +0200

    initial commit with firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'game-notes-zlabidev',
+    appId: '1:518180237640:web:a76ed0f0c977e4cc08c37d',
+    storageBucket: 'game-notes-zlabidev.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyDbmT979mQY4VI6alPbctV44E-r9hKhh8M',
+    authDomain: 'game-notes-zlabidev.firebaseapp.com',
+    messagingSenderId: '518180237640',
+    measurementId: 'G-51X10CDVV9',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'game-notes-zlabidev',
+    appId: '1:518180237640:web:a76ed0f0c977e4cc08c37d',
+    storageBucket: 'game-notes-zlabidev.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyDbmT979mQY4VI6alPbctV44E-r9hKhh8M',
+    authDomain: 'game-notes-zlabidev.firebaseapp.com',
+    messagingSenderId: '518180237640',
+    measurementId: 'G-51X10CDVV9',
+  },
   production: false
 };
 
